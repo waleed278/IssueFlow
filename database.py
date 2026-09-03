54af4d0372cc9fd252759c85038b538bba4b8007
WaleedAhmedAli <waleedahmedali2002@gmail.com>
2026-09-04 01:24:48 +0500

    schema update

--- database.py
+++ database.py
@@ -1,13 +1,20 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Session
+
 
 DATABASE_URL = "sqlite:///./issueflow.db"
 
 
-engine =  create_engine(
+engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread":False}
+    connect_args={"check_same_thread": False}
 )
+
 
 class Base(DeclarativeBase):
     pass
+
+
+def get_db():
+    with Session(engine) as session:
+        yield session
--- main.py
+++ main.py
@@ -1,35 +1,49 @@
 from contextlib import asynccontextmanager
 from typing import Literal
-from sqlalchemy.orm import Session
-from fastapi import FastAPI, HTTPException, status
+
+from fastapi import (
+    Depends,
+    FastAPI,
+    HTTPException,
+    status,
+)
 from pydantic import BaseModel, Field
-import models
-from database import Base,engine
 from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+import models
+from database import Base, engine, get_db
+
 
 
 @asynccontextmanager
-async def lifespan(app:FastAPI):
+async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
     yield
 
-app = FastAPI(lifespan=lifespan)
+
+app = FastAPI(
+    title="IssueFlow API",
+    lifespan=lifespan
+)
+
 
 class TaskCreate(BaseModel):
     title: str = Field(
         min_length=3,
         max_length=100
     )
+
     description: str | None = Field(
         default=None,
         max_length=500
     )
+
     priority: Literal[
         "low",
         "medium",
         "high"
     ]
-
 
 class TaskUpdate(BaseModel):
     title: str | None = Field(
@@ -37,20 +51,25 @@ class TaskUpdate(BaseModel):
         min_length=3,
         max_length=100
     )
+
     description: str | None = Field(
         default=None,
         max_length=500
     )
+
     priority: Literal[
         "low",
         "medium",
         "high"
     ] | None = None
+
     status: Literal[
         "todo",
         "in_progress",
         "done"
     ] | None = None
+
+
 class TaskResponse(BaseModel):
     id: int
     title: str
@@ -70,26 +89,6 @@ class TaskResponse(BaseModel):
         "from_attributes": True
     }
 
-class TaskListResponse(BaseModel):
-    tasks : list[TaskResponse]
-
-tasks = [
-    {
-        "id": 1,
-        "title": "Learn FastAPI",
-        "description": "Understand FastAPI fundamentals",
-        "priority": "high",
-        "status": "todo"
-    },
-    {
-        "id": 2,
-        "title": "Build IssueFlow",
-        "description": "Build our first backend project",
-        "priority": "medium",
-        "status": "in_progress"
-    }
-]
-
 
 @app.get("/")
 def home():
@@ -97,91 +96,24 @@ def home():
         "message": "IssueFlow API is running"
     }
 
-
-@app.get(
-    "/tasks",
-    response_model=list[TaskResponse]
-)
-def get_tasks():
-
-    with Session(engine) as db:
-
-        tasks = db.execute(
-            select(models.Task)
-        ).scalars().all()
-
-        return tasks
-
-@app.get("/tasks/{task_id}",response_model=TaskResponse)
-def get_task(task_id: int):
-
-    with Session(engine) as db:
-        task = db.get(models.Task, task_id)
-
-        if task is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Not Found")
-
-        return task
-
-
 @app.post(
     "/tasks",
     response_model=TaskResponse,
     status_code=status.HTTP_201_CREATED
 )
-def create_task(task: TaskCreate):
-   with Session(engine) as db:
-       db_task = models.Task(
-           title = task.title,
-           description = task.description,
-           priority =  task.priority,
-           status = "todo"
-       )
-
-       db.add(db_task)
-       db.commit()
-       db.refresh(db_task)
-       return db_task
-
-
-@app.patch("/tasks/{task_id}")
-def update_task(
-    task_id: int,
-    task_update: TaskUpdate
+def create_task(
+    task: TaskCreate,
+    db: Session = Depends(get_db)
 ):
-    with Session(engine) as db:
-        db_task = db.get(models.Task,task_id)
-        if db_task is None:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Not Found")
-        update_data = task_update.model_dump(exclude_unset=True)
+    db_task = models.Task(
+        title=task.title,
+        description=task.description,
+        priority=task.priority,
+        status="todo"
+    )
 
-        for fields, values in update_data.items():
-            setattr(db_task,fields,values)
-        db.commit()
-        db.refresh(db_task)
+    db.add(db_task)
+    db.commit()
+    db.refresh(db_task)
 
-        return db_task
-
-
-@app.delete("/tasks/{task_id}")
-def delete_task(task_id: int):
-
-    with Session(engine) as db:
-
-        db_task = db.get(
-            models.Task,
-            task_id
-        )
-
-        if db_task is None:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                detail="Task not found"
-            )
-
-        db.delete(db_task)
-        db.commit()
-
-        return {
-            "message": "Task deleted successfully"
-        }
+    return db_task
