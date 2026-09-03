2c0e024141050fb0c652a6ef8bc572d163d1e7c6
WaleedAhmedAli <waleedahmedali2002@gmail.com>
2026-09-04 02:03:55 +0500

    final

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ import models
 from database import Base, engine, get_db
 
 
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
@@ -44,6 +43,7 @@ class TaskCreate(BaseModel):
         "medium",
         "high"
     ]
+
 
 class TaskUpdate(BaseModel):
     title: str | None = Field(
@@ -96,6 +96,7 @@ def home():
         "message": "IssueFlow API is running"
     }
 
+
 @app.post(
     "/tasks",
     response_model=TaskResponse,
@@ -117,3 +118,100 @@ def create_task(
     db.refresh(db_task)
 
     return db_task
+
+
+@app.get(
+    "/tasks",
+    response_model=list[TaskResponse]
+)
+def get_tasks(
+    db: Session = Depends(get_db)
+):
+    tasks = db.execute(
+        select(models.Task)
+    ).scalars().all()
+
+    return tasks
+
+
+@app.get(
+    "/tasks/{task_id}",
+    response_model=TaskResponse
+)
+def get_task(
+    task_id: int,
+    db: Session = Depends(get_db)
+):
+    task = db.get(
+        models.Task,
+        task_id
+    )
+
+    if task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Task not found"
+        )
+
+    return task
+
+
+@app.patch(
+    "/tasks/{task_id}",
+    response_model=TaskResponse
+)
+def update_task(
+    task_id: int,
+    task_update: TaskUpdate,
+    db: Session = Depends(get_db)
+):
+    db_task = db.get(
+        models.Task,
+        task_id
+    )
+
+    if db_task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Task not found"
+        )
+
+    update_data = task_update.model_dump(
+        exclude_unset=True
+    )
+
+    for field, value in update_data.items():
+        setattr(
+            db_task,
+            field,
+            value
+        )
+
+    db.commit()
+    db.refresh(db_task)
+
+    return db_task
+
+
+@app.delete("/tasks/{task_id}")
+def delete_task(
+    task_id: int,
+    db: Session = Depends(get_db)
+):
+    db_task = db.get(
+        models.Task,
+        task_id
+    )
+
+    if db_task is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Task not found"
+        )
+
+    db.delete(db_task)
+    db.commit()
+
+    return {
+        "message": "Task deleted successfully"
+    }
